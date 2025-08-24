921124f78b5b2f99c140ef4bad1cb3a9dfa74e31
Alain De Long <leholong007@gmail.com>
2025-08-24 17:00:01 +0700

    fix error in main-test file

--- app/main_test.py
+++ app/main_test.py
@@ -7,7 +7,7 @@ class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string: str = "This is my test string"
         random_string_reversed: str = "gnirts tset ym si sihT"
-        self.assertEqual(random_string_reversed, reversed(random_string))
+        self.assertEqual(random_string_reversed, return_backwards_string(random_string))
 
     def test_get_env(self):
         self.assertEqual(os.environ.get("MODE"), get_mode())
