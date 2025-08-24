1c629fd695b3e6f1294109e10b5d71a4cb3c994c
Alain De Long <leholong007@gmail.com>
2025-08-24 17:03:23 +0700

    add type annotations

--- app/main_test.py
+++ app/main_test.py
@@ -4,12 +4,12 @@ import os
 
 
 class TestMain(unittest.TestCase):
-    def test_return_backwards_string(self):
+    def test_return_backwards_string(self) -> None:
         random_string: str = "This is my test string"
         random_string_reversed: str = "gnirts tset ym si sihT"
         self.assertEqual(random_string_reversed, return_backwards_string(random_string))
 
-    def test_get_env(self):
+    def test_get_env(self) -> None:
         self.assertEqual(os.environ.get("MODE"), get_mode())
 
 
